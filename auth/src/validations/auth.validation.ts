adde4a99bfc97c8de4543c52b2694b04215ab8d3
mritik75way <mritik.75way@gmail.com>
2026-01-21 18:02:57 +0530

    feat: update password validation minimum length for registration and login schemas

--- auth/src/validations/auth.validation.ts
+++ auth/src/validations/auth.validation.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 export const registerSchema = z.object({
   name: z.string().min(2),
   email: z.email(),
-  password: z.string().min(4)
+  password: z.string().min(6)
 });
 
 export const loginSchema = z.object({
   email: z.email(),
-  password: z.string().min(1)
+  password: z.string().min(6)
 });
 
 export const forgotPasswordSchema = z.object({
